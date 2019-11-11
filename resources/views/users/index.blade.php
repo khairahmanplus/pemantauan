25a36111dca118a06d6472b7536e19d2717c5e08
Khai Rahman <khairahmanplus@gmail.com>
2019-11-11 14:18:39 +0800

    WIP

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <thead>Nama</thead>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($users as $user)
+                        <tr>
+                            <td>{{ $user ?? '-' }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+// Senaraikan pengguna di dalam pangkalan data
+Route::get('users', function () {
+    // Dapatkan dari database
+    $users = [
+        'Ali',
+        'Abu',
+        null
+    ];
+    // Kembalikan view bersama dengan variable
+    return view('users.index', ['users' => $users]);
+});
+// Kembalikan borang untuk create pengguna baru
+Route::get('users/create', function () {});
+// Memaparkan butiran pengguna
+Route::get('users/{id}', function () {});
